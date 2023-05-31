8d0981fcde720b1fccfc5032ca6bcc1fd99550ac
Tridwan <iamtridwan@gmail.com>
2023-05-31 06:35:36 +0100

    postcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+// import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,19 +8,15 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='font-sans'>Vite + React</h1>
+      <h1 className='font-sans text-red-300 font-semibold'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <p className='text-2xl'>
+          Edit and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
