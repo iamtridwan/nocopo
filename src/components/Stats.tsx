dcfcf0a137b5a8c5f2e0709184f2beba213a5d2a
Tridwan <iamtridwan@gmail.com>
2023-06-01 22:59:02 +0100

    stat updates

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,10 +1,10 @@
 // import React from "react";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement } from "chart.js";
 import { Pie, Bar } from "react-chartjs-2";
-// import "./styles/chart.css";
+import { Data } from "../utils/Data";
+import "./styles/chart.css";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
-
+ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement);
 
 const Stats = () => {
   const data = {
@@ -30,44 +30,33 @@ const Stats = () => {
       },
     ],
   };
-//   const secData = {
-//     labels: [
-//       "Procurement category",
-//       "Procurement category",
-//       "Procurement category",
-//       "Procurement category",
-//     ],
-//     // datasets is an array of objects where each object represents a set of data to display corresponding to the labels above. for brevity, we'll keep it at one object
-//     datasets: [
-//       {
-//         label: "All Contract Groups By Procurement Category",
-//         data: [45, 30, 70, 53],
-//         // you can set indiviual colors for each bar
-//         backgroundColor: [
-//           "rgba(93, 95, 239, 1)",
-//           "rgba(4, 57, 89, 1)",
-//           "rgba(239, 93, 168, 1)",
-//           "rgba(189, 134, 0, 1)",
-//         ],
-//         borderWidth: 1,
-//       },
-//     ],
-//   };
 
-//   const barData = {
-//     labels: Data.map((data) => data.year), 
-//     datasets: [
-//       {
-//         label: "Users Gained ",
-//         data: Data.map((data) => data.userGain),
-//         backgroundColor: [
-//           "rgba(8, 65, 22, 1)"
-//         ],
-//         borderColor: "rgba(8, 65, 22, 1)",
-//         borderWidth: 2
-//       }
-//     ]
-//   }
+
+  const barData = {
+    labels: Data.map(o => o.dept),
+    datasets: [
+      {
+        label: 'Allocated Amount',
+        backgroundColor: 'rgba(8, 65, 22, 1)',
+        borderColor: 'rgb(0, 255, 0)',
+        borderWidth: 1,
+        data: Data.map(o => o.userGain)
+      }
+    ],
+  }
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Project Name",
+      },
+    },
+  };
 
   return (
     <section className="w-full px-6 md:px-16 mb-8 ">
@@ -76,19 +65,19 @@ const Stats = () => {
       </h1>
       <div className="flex items-center justify-center flex-col md:grid md:grid-cols-2 gap-4 md:gap-10 mt-8">
         <div
-          className="bg-white rounded-xl flex items-center justify-center py-4 md:py-10"
+          className="chart bg-white rounded-xl flex items-center justify-center py-4 md:py-10"
           style={boxStyles}
         >
           <Pie data={data} />
         </div>
         <div
-          className="bg-white rounded-xl flex items-center justify-center py-4 md:py-10"
+          className="chart bg-white rounded-xl flex items-center justify-center py-4 md:py-10"
           style={boxStyles}
         >
-         <Pie data={data} />
+          <Pie data={data} />
         </div>
         <div
-          className="md:col-span-2 mt-4 bg-white rounded-xl flex items-center justify-between flex-col md:flex-row py-4 md:py-10 px-4 md:px-8 z-20 h-[50vh] -mb-52 md:-mb-0"
+          className="md:col-span-2 mt-4 bg-white rounded-xl flex items-center justify-between flex-col md:flex-row py-4 md:py-10 px-4 md:px-8 z-20 -mb-52 md:-mb-0"
           style={boxStyles}
         >
           <aside className="w-64 md:mr-10">
@@ -100,8 +89,8 @@ const Stats = () => {
               aloocated budget and contract period
             </p>
           </aside>
-          <div className="flex-1 w-full">
-            
+          <div className="bar flex-1 w-full flex items-center justify-center">
+            <Bar options={options} data={barData} />
           </div>
         </div>
       </div>
--- src/utils/Data.ts
+++ src/utils/Data.ts
@@ -1,32 +1,37 @@
 export const Data = [
     {
       id: 1,
-      year: 2016,
+      dept:"Education ministry",
+      year: 2015,
       userGain: 80000,
       userLost: 823
     },
     {
       id: 2,
+      dept:"Power ministry",
       year: 2017,
       userGain: 45677,
       userLost: 345
     },
     {
       id: 3,
-      year: 2018,
+      dept:"Finance ministry",
       userGain: 78888,
+      year: 2018,
       userLost: 555
     },
     {
       id: 4,
-      year: 2019,
+      dept:"Agric ministry",
       userGain: 90000,
+      year: 2019,
       userLost: 4555
     },
     {
       id: 5,
-      year: 2020,
+      dept:"Road ministry",
       userGain: 4300,
+      year: 2020,
       userLost: 234
     }
   ];
